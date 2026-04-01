395f3ffd6ca196d9e2b0e96364a3a643f0ed22ce
Owner <owner@Owners-MacBook-Air.local>
2026-03-31 23:52:52 -0400

    Blackout: Removing template frame

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,22 @@
-import { CartProvider } from "components/cart/cart-context";
-import { Navbar } from "components/layout/navbar";
-import { WelcomeToast } from "components/welcome-toast";
 import { GeistSans } from "geist/font/sans";
-import { getCart } from "lib/shopify";
 import { ReactNode } from "react";
-import { Toaster } from "sonner";
 import "./globals.css";
-import { baseUrl } from "lib/utils";
-
-const { SITE_NAME } = process.env;
 
 export const metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: SITE_NAME!,
-    template: `%s | ${SITE_NAME}`,
-  },
-  robots: {
-    follow: true,
-    index: true,
-  },
+  title: "CCTRDG // REV-01",
+  description: "Counter-Clockwise R&D Group",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  // Don't await the fetch, pass the Promise to the context provider
-  const cart = getCart();
-
   return (
     <html lang="en" className={GeistSans.variable}>
-      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        <CartProvider cartPromise={cart}>
-          <Navbar />
-          <main>
-            {children}
-            <Toaster closeButton />
-            <WelcomeToast />
-          </main>
-        </CartProvider>
+      {/* We removed the white background and default Navbar here */}
+      <body className="bg-black text-white antialiased">
+        {children}
       </body>
     </html>
   );
